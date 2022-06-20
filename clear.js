add434ed3e3ff2e5ed1ce3e8f169fb77844889fa
Adrien Varricchione <47208777+Shedhatch@users.noreply.github.com>
2022-06-20 15:32:02 +0200

    Updated Clear.js
    - Code has been formatted
    - New comments added
    - Removed all comments that were inside the module
    - Improved English in returns strings

--- clear.js
+++ clear.js
@@ -1,27 +1,25 @@
-
-  module.exports.run = async (bot, message, args) => {
-
-
-    
-    if(message.member.hasPermission("ADMINISTRATOR")) {     ///To allow a simple moderator to use the command just put MANAGE_MESSAGES in place of ADMINISTRATOR 
-      let messagecount = parseInt(args[0]);
-    
-      if(isNaN(messagecount)) return message.channel.send(":x: " + "| Please mention the amount of message that you want to delete");
-    
-      if(messagecount > 100){
-        message.channel.send(":x: " + "| Error, you can only delete between 2 and 100 messages at one time !")
-      }else if(messagecount < 2 ) {
-        message.channel.send(":x: " + "| Error, you can only delete between 2 and 100 messages at one time !")
-      } else {
-    
-      }{
-        message.channel.fetchMessages({limit: messagecount}).then(messages => message.channel.bulkDelete(messages, true));
-      }
+module.exports.run = async (bot, message, args) => {
+  if(message.member.hasPermission("ADMINISTRATOR")) {
+    let messagecount = parseInt(args[0]);
+    if(isNaN(messagecount)) return message.channel.send(":x: " + "| Please indicate the amount of message that you want to delete.");
+    if(messagecount > 100){
+      message.channel.send(":x: " + "| Error, you can only delete between 2 and 100 messages at one time !")
+    }else if(messagecount < 2 ) {
+      message.channel.send(":x: " + "| Error, you can only delete between 2 and 100 messages at one time !")
     } else {
-      return message.reply(":x: " + "| You need to be \"ADMINISTRATOR\" to do that")
+    }{
+      message.channel.fetchMessages({limit: messagecount}).then(messages => message.channel.bulkDelete(messages, true));
     }
-    }
-    
-    module.exports.help = {
-        name: "clear"
-    }
+  } else {
+    return message.reply(":x: " + "| You need to be \"ADMINISTRATOR\" to use this command.")
+  }
+}
+
+module.exports.help = {
+  name: "clear"
+}
+
+// Comments: 
+// messagecount Max amount has been set to 100
+// messagecount Min amount has been set to 2
+// This command is only allowed to be used by Administrators check to learn about role permissions: https://discordjs.guide/popular-topics/permissions.html#roles-as-bot-permissions
